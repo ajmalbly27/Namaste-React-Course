cebe8aebe76bffcb12eff76f68b415f157d452cf
MohdAjmalRaza <ajmalbly27@gmail.com>
2024-09-19 14:15:05 +0530

    Assignment of Episode 10 is completed

---
+++ Ep10-Jo Dikhta Hai, Vo Bikta Hai/Assignment/Coding Assignment/src/components/Header.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const onlineStatus = useOnlineStatus();
+
+  return (
+    <div className="flex justify-between items-center bg-pink-50">
+      <div>
+        <img className="w-28 m-3" src={LOGO_URL} />
+      </div>
+      <div className="w-[50%]">
+        <ul className="flex w-[100%] justify-around">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Ep10-Jo Dikhta Hai, Vo Bikta Hai/Assignment/Coding Assignment/src/components/Shimmer.js
@@ -0,0 +1,32 @@
+const Shimmer = () => {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+      <div className="w-56 h-80 m-4 bg-gray-200"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
